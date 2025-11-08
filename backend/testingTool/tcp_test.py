a5ca2f942b7511f9417ca2e003709f5f3457d2fa
Surigoma <negi.1994.t.taku.1216@gmail.com>
2025-11-08 22:52:44 +0900

    Update test tool for tcp

--- backend/testingTool/tcp_test.py
+++ backend/testingTool/tcp_test.py
@@ -1,9 +1,14 @@
 import socket
+import sys
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+if len(sys.argv) > 1:
+    msg = " ".join(sys.argv[1:])
+else:
+    msg = input("cmd>")
 client.connect(("127.0.0.1", 50000))
-client.send(b"test")
+client.send(msg.encode("utf-8"))
 resp = client.recv(512)
 print(resp)
 client.close()
