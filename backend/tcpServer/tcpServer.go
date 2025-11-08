60b8c5d7a4a9f109d9181751720a6b19175d52c5
Surigoma <negi.1994.t.taku.1216@gmail.com>
2025-11-08 22:52:25 +0900

    Support TCP message for "fadeIn/Out" and "mute"

--- backend/tcpServer/tcpServer.go
+++ backend/tcpServer/tcpServer.go
@@ -9,6 +9,7 @@ import (
 	"log/slog"
 	"net"
 	"regexp"
+	"strings"
 	"sync"
 	"time"
 )
@@ -17,6 +18,7 @@ var logger *slog.Logger
 var listenAddr *net.TCPAddr
 var wg *sync.WaitGroup
 var running bool = false
+var v1Msgs map[string][]string
 
 var TcpServer packageModule.PackageModule = packageModule.PackageModule{
 	ModuleName:     "tcp",
@@ -30,6 +32,7 @@ func Initialize(module *packageModule.PackageModule, config *config.Config) bool
 	logger = module.Logger
 	wg = module.Wg
 	running = true
+	v1Msgs = makeV1Messages(config)
 	listenAddr, err = net.ResolveTCPAddr("tcp", fmt.Sprintf("%s:%d", config.Tcp.IP, config.Tcp.Port))
 	if err != nil {
 		logger.Error("Failed to setup TCP", "error", err)
@@ -37,21 +40,89 @@ func Initialize(module *packageModule.PackageModule, config *config.Config) bool
 	}
 	return true
 }
+
+func makeV1Messages(config *config.Config) map[string][]string {
+	result := make(map[string][]string)
+	if _, ok := config.Dmx.Groups["stg"]; ok {
+		result["fi"] = []string{"fadeIn", "stg"}
+		result["fo"] = []string{"fadeOut", "stg"}
+		result["ci"] = []string{"fadeIn", "stg", "interval:0"}
+		result["co"] = []string{"fadeOut", "stg", "interval:0"}
+	}
+	if _, ok := config.Dmx.Groups["aud"]; ok {
+		result["fai"] = []string{"fadeIn", "aud"}
+		result["fao"] = []string{"fadeOut", "aud"}
+	}
+	result["mute"] = []string{"mute", "true"}
+	result["unmute"] = []string{"mute", "false"}
+	return result
+}
+
 func handleRequest(conn *net.TCPConn) {
 	logger.Info("Connect", "remote", conn.RemoteAddr())
 	defer conn.Close()
 	buf := make([]byte, 512)
 	for {
-		len, err := conn.Read(buf)
+		l, err := conn.Read(buf)
 		if err != nil {
 			if err != io.EOF {
 				logger.Warn("Invalid close", "error", err)
 			}
 			break
 		}
-		msgs := regexp.MustCompile("\r\n|\n|\r").Split(string(buf[:len]), -1)
+		msgs := regexp.MustCompile("\r\n|\n|\r").Split(string(buf[:l]), -1)
 		for _, msg := range msgs {
-			switch msg {
+			cmd := strings.Split(msg, " ")
+			if v1Msgs != nil && len(cmd) == 1 {
+				for key, newCmd := range v1Msgs {
+					if cmd[0] == key {
+						cmd = newCmd
+						break
+					}
+				}
+			}
+			logger.Debug("TCP message", "cmd", cmd)
+			switch cmd[0] {
+			case "fadeIn", "fadeOut":
+				isIn := cmd[0] == "fadeIn"
+				msgArg := message.MessageBody{
+					Action: "fade",
+					Arg:    map[string]string{},
+				}
+				if len(cmd) <= 1 {
+					continue
+				}
+				if len(cmd) >= 3 {
+					args := strings.Split(cmd[2], ",")
+					for _, v := range args {
+						if !strings.Contains(v, ":") {
+							continue
+						}
+						arg := strings.Split(v, ":")
+						logger.Debug("test", "arg", arg)
+						msgArg.Arg[arg[0]] = arg[1]
+					}
+				}
+				msgArg.Arg["id"] = cmd[1]
+				msgArg.Arg["isIn"] = fmt.Sprintf("%v", isIn)
+				go packageModule.ModuleManager.SendMessage(message.Message{
+					To:  "dmx",
+					Arg: msgArg,
+				})
+			case "mute":
+				mute := true
+				if len(cmd) >= 2 && cmd[1] == "false" {
+					mute = false
+				}
+				go packageModule.ModuleManager.SendMessage(message.Message{
+					To: "osc",
+					Arg: message.MessageBody{
+						Action: "mute",
+						Arg: map[string]string{
+							"mute": fmt.Sprintf("%v", mute),
+						},
+					},
+				})
 			case "test":
 				logger.Debug("test")
 				go packageModule.ModuleManager.SendMessage(message.Message{
